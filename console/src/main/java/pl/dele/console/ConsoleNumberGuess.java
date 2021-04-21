4fed73578c9506af1888c99a1ceb56d550522a1a
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-21 18:33:34 +0200

    add ConsoleNumberGuess full impl

--- console/src/main/java/pl/dele/console/ConsoleNumberGuess.java
+++ console/src/main/java/pl/dele/console/ConsoleNumberGuess.java
@@ -2,9 +2,14 @@ package pl.dele.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import pl.dele.Game;
+import pl.dele.MessageGenerator;
+
+import java.util.Scanner;
 
 @Component
 public class ConsoleNumberGuess{
@@ -12,8 +17,47 @@ public class ConsoleNumberGuess{
     // == constants ==
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
+    // == fields ==
+    @Autowired
+    private Game game;
+
+    @Autowired
+    private MessageGenerator messageGenerator;
+
+    // == events ==
     @EventListener(ContextRefreshedEvent.class)
     public void start() {
         log.info("start() --> Container ready to use.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            // get input from the user
+            while(!scanner.hasNextInt()){
+                scanner.nextLine();
+            }
+            int guess = scanner.nextInt();
+            scanner.nextLine();
+
+            game.setGuess(guess);
+            game.check();
+
+            // result
+            if(game.isGameWon() || game.isGameLost()){
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("Play again y/n?");
+
+                String playAgainString = scanner.nextLine().trim();
+                if(!playAgainString.equalsIgnoreCase("y")){
+                    scanner.close();
+                    break;
+                }
+
+                game.reset();
+            }
+        }
     }
 }
