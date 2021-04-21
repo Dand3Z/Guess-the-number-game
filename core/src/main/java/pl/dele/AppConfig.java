8b3e5eb36e5b8b8b8c0291e2ce3682af82a4fd8d
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-21 15:04:20 +0200

    move beans config from xml to AppConfig class

---
+++ core/src/main/java/pl/dele/AppConfig.java
@@ -0,0 +1,13 @@
+package pl.dele;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * configuration for the spring container
+ */
+@Configuration
+@ComponentScan(basePackages = "pl.dele")
+public class AppConfig {
+
+}
