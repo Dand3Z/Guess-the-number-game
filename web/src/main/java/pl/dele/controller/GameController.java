bcec4976bcafbfb8f04e9a8a1f8f73cb8b09065f
Daniel Delegacz <ddelegacz@gmail.com>
2021-05-02 15:45:39 +0200

    add Thymeleaf pages

---
+++ web/src/main/java/pl/dele/controller/GameController.java
@@ -0,0 +1,46 @@
+package pl.dele.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.dele.service.GameService;
+import pl.dele.util.AttributeNames;
+import pl.dele.util.GamesMappings;
+import pl.dele.util.ViewNames;
+
+@Slf4j
+@Controller
+public class GameController {
+
+    // == fields ==
+    private final GameService gameService;
+
+    // constructors ==
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    // == request methods ==
+    @GetMapping(GamesMappings.PLAY)
+    public String play(Model model){
+        model.addAttribute(AttributeNames.MAIN_MESSAGE, gameService.getMainMessage());
+        model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMessage());
+        log.info("model = {}", model);
+
+        if (gameService.isGameOver()) return ViewNames.GAME_OVER;
+        else return ViewNames.PLAY;
+    }
+
+    // Submit button
+    @PostMapping(GamesMappings.PLAY)
+    public String processMessage(@RequestParam int guess){
+        log.info("guess = {}", guess);
+        gameService.checkGuess(guess);
+        return GamesMappings.REDIRECT_PLAY;
+    }
+}
---
+++ web/src/main/java/pl/dele/util/GamesMappings.java
@@ -0,0 +1,11 @@
+package pl.dele.util;
+
+public final class GamesMappings {
+
+    // == constants ==
+    public static final String PLAY = "play";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+
+    // == constructors ==
+    private GamesMappings() {}
+}
