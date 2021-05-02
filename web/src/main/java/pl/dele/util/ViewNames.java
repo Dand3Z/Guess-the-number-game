4e0058905126ebbad92862aa6f6e97aa091afd6c
Daniel Delegacz <ddelegacz@gmail.com>
2021-05-02 13:37:59 +0200

    setup Thymeleaf

---
+++ web/src/main/java/pl/dele/util/ViewNames.java
@@ -0,0 +1,10 @@
+package pl.dele.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames(){}
+}
